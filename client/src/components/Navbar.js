863fa708d6a6f120a4ff0c82681bcbb483918b70
sankaire <kerenketepela@gmail.com>
2022-11-20 16:43:48 +0300

    Feat:create Nav component

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,24 @@
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <div className="nav__left">
+        <h1 className="logo">Symentical</h1>
+      </div>
+      <div className="nav__right">
+        <ul>
+          <li>
+            <a>About</a>
+          </li>
+          <li>
+            <a>Features</a>
+          </li>
+          <li>
+            <a>Contact</a>
+          </li>
+        </ul>
+      </div>
+      <button className="btn">Dashbord</button>
+    </nav>
+  );
+};
+export default Navbar;
