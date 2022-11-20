fa7f8e66221c4f6d7c60c0909e23aa6912d01190
sankaire <kerenketepela@gmail.com>
2022-11-20 16:45:44 +0300

    Feat: reander components

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,12 @@
 import { render } from "react-dom";
-import axios from "axios";
+import Navbar from "./components/Navbar";
+import LandingPage from "./sections/Landing_page";
 const App = () => {
-  const getData = async () => {
-    try {
-      const data = await axios.get("https://umbrella-2gw5.onrender.com/api/v1");
-      const res = data.data;
-      console.log(res);
-      setMessage(res.message);
-    } catch (error) {
-      console.log(error.response);
-    }
-  };
   return (
-    <div className="test">
-      <h1>Hello</h1>
-      <button onClick={getData}>Click</button>
-    </div>
+    <>
+      <Navbar />
+      <LandingPage />
+    </>
   );
 };
-
 render(<App />, document.getElementById("root"));
