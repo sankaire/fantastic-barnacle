024e8d5d736de3f690354ff4699dd17cacbb2d3b
sankaire <kerenketepela@gmail.com>
2022-11-21 18:10:37 +0300

    Feat:add login controller

--- server/controllers/auth.controller.ts
+++ server/controllers/auth.controller.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
-import { createUser } from "../services/auth.service";
+import { createUser, login } from "../services/auth.service";
 
 export const signUp = async (req: Request, res: Response): Promise<object> => {
-  const { username, email, phone, password } = req.body;
+  const { username, email, company, password } = req.body;
   try {
-    const user = await createUser(username, email, phone, password);
+    const user = await createUser(username, email, company, password);
     if (!user) {
       return res.status(400).json({ message: "user not created" });
     }
@@ -13,3 +13,16 @@ export const signUp = async (req: Request, res: Response): Promise<object> => {
     return res.status(500).json({ error: error.message });
   }
 };
+export const signIn =async (req:Request, res:Response):Promise<object> => {
+  const {email, password} = req.body
+  try{
+    const resp:any = await login(email, password)
+    if(resp.email === undefined){
+      return res.status(500).json(resp)
+    }
+    return res.status(200).json(resp)
+
+  }catch(error:any){
+    return res.status(500).json({error:error.message})
+  }
+}
