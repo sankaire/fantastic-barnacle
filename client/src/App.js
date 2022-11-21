840fc29cf5ac2ebceccc4471b68920228e081cb4
sankaire <kerenketepela@gmail.com>
2022-11-21 18:06:14 +0300

    Feat:add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import { render } from "react-dom";
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Dashbord from "./dashboard/Dashboard";
 import LandingPage from "./sections/Landing_page";
+const user = localStorage.getItem("user");
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <LandingPage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/signup"
+          element={!user ? <Signup /> : <Navigate to="/login" />}
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={!user ? <Login /> : <Dashbord />} />
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 render(<App />, document.getElementById("root"));
