b9deeb52416d1a35d4fe4bcd8bb24e0e80e32057
sankaire <kerenketepela@gmail.com>
2022-11-21 18:04:53 +0300

    Feat:render navbar on landing page

--- client/src/sections/Landing_page.js
+++ client/src/sections/Landing_page.js
@@ -1,16 +1,20 @@
 import homeimg from "../../images/home.svg";
+import Navbar from "../components/Navbar";
 
 const LandingPage = () => {
   return (
-    <div className="landing__page">
-      <span>
-        We are the <h2>Best</h2>
-        Plartform to <h2>manage</h2> your Store and <h2>Inventories</h2>
-      </span>
-      <span>
-        <img clasName="home_image" src={homeimg} alt="pic" />
-      </span>
-    </div>
+    <>
+      <Navbar />
+      <div className="landing__page">
+        <span>
+          We are the <h2>Best</h2>
+          Plartform to <h2>manage</h2> your Store and <h2>Inventories</h2>
+        </span>
+        <span>
+          <img clasName="home_image" src={homeimg} alt="pic" />
+        </span>
+      </div>
+    </>
   );
 };
 export default LandingPage;
