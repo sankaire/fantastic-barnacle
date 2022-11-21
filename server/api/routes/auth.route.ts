d9e69511d8a02a473f462a0db8a01877b7481321
sankaire <kerenketepela@gmail.com>
2022-11-21 18:08:26 +0300

    Feat:create login endpoint

--- server/api/routes/auth.route.ts
+++ server/api/routes/auth.route.ts
@@ -1,8 +1,9 @@
 import express from 'express'
-import {signUp} from '../../controllers/auth.controller'
+import {signIn, signUp} from '../../controllers/auth.controller'
 const router = express.Router()
 
 router.post("/auth/signup", signUp)
+router.post("/auth/login", signIn)
 
 const authRouter = router
 export default authRouter
