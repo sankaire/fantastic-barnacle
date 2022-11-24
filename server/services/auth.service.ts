2c14a65a5046971bb094610903dddaf330a9ce00
sankaire <kerenketepela@gmail.com>
2022-11-24 14:12:38 +0300

    Fix:validate password, email and company

--- server/services/auth.service.ts
+++ server/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { User } from "../schemas/Users";
-import bcrypt from "bcrypt";
 import createToken from "../utils/jwt";
+import bcrypt from "bcryptjs";
 
 export const createUser = async (
   username: string,
@@ -8,7 +8,7 @@ export const createUser = async (
   company: string,
   password: string
 ): Promise<object> => {
-  const hash = bcrypt.hashSync(password, 10)
+  const hash = await bcrypt.hash(password, 10);
   const newUser = new User({
     username: username,
     email: email,
@@ -17,20 +17,44 @@ export const createUser = async (
   });
   const user = await newUser.save();
   const token = createToken(user._id.toString());
-  return {user, token}
+  return { user, token };
 };
 
-export const login = async(email:string, password:string):Promise<object> =>{
-  const user:any = await User.findOne({email:email})
-  if(!user){
-    return {message:"User not found. create an account"}
+export const login = async (
+  email: string,
+  password: string
+): Promise<object> => {
+  const user: any = await User.findOne({ email: email });
+  if (!user) {
+    return { message: "User not found. create an account" };
   }
-  const savedPass = user.password
-  // const comparePass = bcrypt.compareSync(password,savedPass)
-  // console.log(comparePass)
-  // if(!comparePass){
-  //   const error ={message:"Wrong password"}
-  //   return error
-  // }
-  return user
-}
+  const savedPass = user.password;
+  const comparePass = await bcrypt.compare(password, savedPass);
+  if (!comparePass) {
+    const error = { message: "wrong password" };
+    return error;
+  }
+  return user;
+};
+export const checkEmail = async (email: string): Promise<boolean> => {
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return false;
+    }
+    return true;
+  } catch (error: any) {
+    return error;
+  }
+};
+export const CheckCompany = async (company: string): Promise<boolean> => {
+  try {
+    const user = await User.findOne({ company: company });
+    if (!user) {
+      return false;
+    }
+    return true;
+  } catch (error: any) {
+    return error;
+  }
+};
